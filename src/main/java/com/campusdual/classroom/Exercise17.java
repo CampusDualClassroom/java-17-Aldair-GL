79cc40bb116ca49d360659595885b2155964b480
Aldair-GL <alejandro555azpeitia@gmail.com>
2025-05-25 18:22:17 +0200

    Exercise 17 solved.

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,31 @@
 package com.campusdual.classroom;
 
+import java.util.Arrays;
+
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	public static int[] createEmptyIntArray(int positions) {
+		return new int[positions];
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static String[] createInitializedStringArray() {
+		String[] arrayString = new String[3];
+		arrayString[0]="1";
+		arrayString[1]="2";
+		arrayString[2]="3";
+		return arrayString;
+	}
 
-	public static void populateArray(int[] array, int index, int value) {}
+	public static void populateArray(int[] array, int index, int value) {
+		array[index]=value;
+	}
 
-	public static int arrayLength(int[] array) {}
+	public static int arrayLength(int[] array) {
+		return array.length;
+	}
 
-	public static void showValue(int[] array, int index) {}
+	public static void showValue(int[] array, int index) {
+		System.out.println(array[index]);
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
@@ -19,5 +35,7 @@ public class Exercise17 {
 		populateArray(intArray, 2, 8);
 		showValue(intArray, 0);
 		System.out.println(arrayLength(intArray));
+		System.out.println(Arrays.toString(stringArray));
 	}
+
 }
